1bda2ac2f96f74235b28f89982da5330f581d384
aluqard <aluqard@gmail.com>
2024-10-07 23:50:20 +0900

    add manifest.py file and change configurations for WAVESHARE board

--- builder/esp32.py
+++ builder/esp32.py
@@ -190,7 +190,7 @@ def common_args(extra_args):
         def_flash_size = 8
     elif board in (
         'UM_FEATHERS2', 'SIL_WESP32',
-        'UM_PROS3', 'UM_FEATHERS3',
+        'UM_PROS3', 'UM_FEATHERS3', 'T-HMI', 'ESP32_WAVESHARE'
     ):
         def_flash_size = 16
     else:
@@ -422,6 +422,8 @@ def parse_args(extra_args, lv_cflags, brd):
     elif board == 'UM_FEATHERS3':
         extra_args = esp32_s3_args(extra_args)
     elif board == 'T-HMI':
+        extra_args = esp32_s3_args(extra_args)
+    elif board == 'ESP32_WAVESHARE':
         extra_args = esp32_s3_args(extra_args)
     elif board == 'ESP32_GENERIC_C3':
         extra_args = esp32_c3_args(extra_args)
