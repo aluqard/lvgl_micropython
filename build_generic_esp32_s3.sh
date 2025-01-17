9859346e51619727ff4ccf6cf2641312446afee6
aluqard <aluqard@gmail.com>
2025-01-17 13:37:19 +0900

    build script 수정

--- build_generic_esp32_s3.sh
+++ build_generic_esp32_s3.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 sudo apt-get install git wget flex bison gperf python3 python3-pip python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0 pkg-config
 cd ext_mod
-if [ -d usqlite ]; then
+if [ ! -d usqlite ]; then
+  git clone https://github.com/spatialdude/usqlite.git
   cd usqlite
   sed -i 's/\^esp32/\^esp32*/g' micropython.cmake
   sed -i '/mtext-section-literals/a \\t\t-DSQLITE_DEBUG' micropython.cmake
   cd ..
+fi
+if [ ! -d micropython-ulab ]; then
+ git clone https://github.com/v923z/micropython-ulab.git
 fi
 cd ..
 cp -R font/* lib/lvgl/src/font
