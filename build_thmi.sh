b71aa21a356931f13e7e09d30c0d77d4e5e17828
aluqard <aluqard@gmail.com>
2025-09-13 23:49:53 +0900

    modified build script for T-HMI

--- build_thmi.sh
+++ build_thmi.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt-get install git wget flex bison gperf python3 python3-pip python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0 pkg-config
-
 cd ext_mod
 if [ -d usqlite ]; then
   rm -rf usqlite
@@ -17,8 +16,9 @@ fi
 
 git clone https://github.com/v923z/micropython-ulab.git
 cd ..
-#cp -R font/* lib/lvgl/src/font
-#yes | cp -R boards/Polverine lib/micropython/ports/esp32/boards 
 
-rm -rf lib/micropython/ports/esp32/build-Polverine
-python3 make.py esp32 clean BOARD=Polverine FROZEN_MANIFEST=$(pwd)/lib/micropython/ports/esp32/boards/Polverine/manifest.py --dual-core-threads --enable-cdc-repl=y
+#cp -R font/* lib/lvgl/src/font
+#yes | cp -R boards/T_DISPLAYS3 lib/micropython/ports/esp32/boards 
+
+rm -rf lib/micropython/ports/esp32/build-T_HMI
+python3 make.py esp32 clean BOARD=T_HMI DISPLAY=st7789 INDEV=xpt2046 FROZEN_MANIFEST=$(pwd)/boards/T_HMI/manifest.py --dual-core-threads --enable-cdc-repl=y
