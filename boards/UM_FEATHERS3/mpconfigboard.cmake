dc540aac0f159aa5f8478e3daab0900239fa6088
aluqard <aluqard@gmail.com>
2025-12-18 11:34:15 +0900

    modify .cmake and .board files to fix build error.

--- boards/UM_FEATHERS3/mpconfigboard.cmake
+++ boards/UM_FEATHERS3/mpconfigboard.cmake
@@ -2,12 +2,9 @@ set(IDF_TARGET esp32s3)
 
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
-    ${SDKCONFIG_IDF_VERSION_SPECIFIC}
-    boards/sdkconfig.usb
     boards/sdkconfig.ble
     boards/sdkconfig.240mhz
     boards/sdkconfig.spiram_sx
-    boards/sdkconfig.spiram
     boards/UM_FEATHERS3/sdkconfig.board
 )
 
